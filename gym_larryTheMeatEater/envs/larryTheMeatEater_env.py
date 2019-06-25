ad9d05aced91c52155b7b1a5f0aa9efabdbb3a51
Adam Olsson <olsson.95_@hotmail.com>
2019-06-25 16:09:43 +0200

    Updated playing field

--- gym_larryTheMeatEater/envs/larryTheMeatEater_env.py
+++ gym_larryTheMeatEater/envs/larryTheMeatEater_env.py
@@ -12,51 +12,52 @@ class larryTheMeatEater_env(gym.Env):
 		# World
 		self.world_size = (8,8)
 		self.world = None
-		self.start_pos = (0,0)
-		self.end_pos = [7,7]
-		self.death_objects_pos = []
+		self.start_pos = 0
+		self.end_pos = self.world_size[0]*self.world_size[1]
+		#self.death_objects_pos = []
 		self.player_pos = self.start_pos
 		self.player_previous_pos = None
 
 		# RL params
-		self.game_over = False
-		self.reward = 0
+		#self.game_over = False
+		#self.reward = 0
 
 		# printing
-		self.flatten = lambda l: [item for sublist in l for item in sublist]
+		#self.flatten = lambda l: [item for sublist in l for item in sublist]
 
 
 	def step(self, action):
 		# returns a list containing new state, the reward for current state, a bool saying game over and 
 		# additional info of our problem
 		if action == 'u':
-			self.player_pos += (0,1)
+			self.player_pos += 8
 		elif action == 'd':
-			self.player_pos += (0,-1)
+			self.player_pos += -8
 		elif action == 'l':
-			self.player_pos += (-1,0)
+			self.player_pos += 1
 		elif action == 'r':
-			self.player_pos += (0,1)
+			self.player_pos += -1
 		
-		self.world[self.player_pos[0]][[self.player_pos[1]]] = 'H'
-		self.world[self.player_previous_pos[0]][self.player_previous_pos[1]] = ' '
+		#self.world[self.player_pos[0]][[self.player_pos[1]]] = 'H'
+		#self.world[self.player_previous_pos[0]][self.player_previous_pos[1]] = ' '
 
 		return [self.world, self.reward, self.game_over, None]
 
 	def reset(self):
 		#resets to state
 		self.world = []
-		for y in range(self.world_size[0]):
-			self.world.append([])
-			for x in range(self.world_size[1]):
-				self.world[y].append(' ')
+		for x in range(self.world_size[0]*self.world_size[1]):
+			self.world.append([' '])
 
 		self.player_pos = self.start_pos
-		self.world[self.end_pos[0]][self.end_pos[1]] = 'R' # R for reward
-		self.world[self.player_pos[0]][self.player_pos[1]] = 'H'
-		self.game_over = False
-		self.reward = 0
-		self.player_previous_pos = None
+		self.player_previous_pos = self.player_pos
+		self.world[self.player_pos] = 'H'
+
+		#self.world[self.end_pos[0]][self.end_pos[1]] = 'R' # R for reward
+		
+		#self.game_over = False
+		#self.reward = 0
+		
 
 		return self.world
 
@@ -67,7 +68,11 @@ class larryTheMeatEater_env(gym.Env):
 		#		print("[{}]".format(x), end ='')
 		#	print('\n')
 
-		flat = self.flatten(self.world)
+		#flat = self.flatten(self.world)
+
+		self.world[self.player_previous_pos] = ' '
+		self.world[self.player_pos] = 'H'
+		self.player_previous_pos = self.player_pos
 
 		# This will make the output overwritten in the terminal
 		print(	"[{}][{}][{}][{}][{}][{}][{}][{}]\n"
@@ -78,14 +83,14 @@ class larryTheMeatEater_env(gym.Env):
 				"[{}][{}][{}][{}][{}][{}][{}][{}]\n"
 				"[{}][{}][{}][{}][{}][{}][{}][{}]\n"
 				"[{}][{}][{}][{}][{}][{}][{}][{}]\n".format(
-					flat[56],flat[57],flat[58],flat[59],flat[60],flat[61],flat[62],flat[63],
-					flat[48],flat[49],flat[50],flat[51],flat[52],flat[53],flat[54],flat[55],
-					flat[40],flat[41],flat[42],flat[43],flat[44],flat[45],flat[46],flat[47],
-					flat[32],flat[33],flat[34],flat[35],flat[36],flat[37],flat[38],flat[39],
-					flat[24],flat[25],flat[26],flat[27],flat[28],flat[29],flat[30],flat[31],
-					flat[16],flat[17],flat[18],flat[19],flat[20],flat[21],flat[22],flat[23],
-					flat[8] ,flat[9] ,flat[10],flat[11],flat[12],flat[13],flat[14],flat[15],
-					flat[0] ,flat[1] ,flat[2] ,flat[3] ,flat[4] ,flat[5] ,flat[6] ,flat[7],), end='\r')
+					self.world[56],self.world[57],self.world[58],self.world[59],self.world[60],self.world[61],self.world[62],self.world[63],
+					self.world[48],self.world[49],self.world[50],self.world[51],self.world[52],self.world[53],self.world[54],self.world[55],
+					self.world[40],self.world[41],self.world[42],self.world[43],self.world[44],self.world[45],self.world[46],self.world[47],
+					self.world[32],self.world[33],self.world[34],self.world[35],self.world[36],self.world[37],self.world[38],self.world[39],
+					self.world[24],self.world[25],self.world[26],self.world[27],self.world[28],self.world[29],self.world[30],self.world[31],
+					self.world[16],self.world[17],self.world[18],self.world[19],self.world[20],self.world[21],self.world[22],self.world[23],
+					self.world[8] ,self.world[9] ,self.world[10],self.world[11],self.world[12],self.world[13],self.world[14],self.world[15],
+					self.world[0] ,self.world[1] ,self.world[2] ,self.world[3] ,self.world[4] ,self.world[5] ,self.world[6] ,self.world[7],), end='\r')
 
 if __name__=='__main__':
 	env = larryTheMeatEater_env()
