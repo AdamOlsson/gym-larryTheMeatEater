1159fd242bfb35d0a26d236de86300c17139fd96
Adam Olsson <olsson.95_@hotmail.com>
2019-06-25 18:36:35 +0200

    Movable user cmd line interface

--- gym_larryTheMeatEater/envs/larryTheMeatEater_env.py
+++ gym_larryTheMeatEater/envs/larryTheMeatEater_env.py
@@ -1,7 +1,7 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
-import keyboard
+import keyboard, time, curses
 #import arcade
 
 class larryTheMeatEater_env(gym.Env):
@@ -19,35 +19,48 @@ class larryTheMeatEater_env(gym.Env):
 		self.player_previous_pos = None
 
 		# RL params
-		#self.game_over = False
-		#self.reward = 0
+		self.game_over = False
+		self.reward = 0
 
 		# printing
-		#self.flatten = lambda l: [item for sublist in l for item in sublist]
+		curses.initscr()
+		self.screen = curses.newwin(10,10)
+		self.screen.keypad(True)
+		curses.noecho()
+		curses.cbreak()
 
 
 	def step(self, action):
+
+		action = self.screen.getkey() # temporary for testing
+
 		# returns a list containing new state, the reward for current state, a bool saying game over and 
 		# additional info of our problem
-		if action == 'u':
-			self.player_pos += 8
-		elif action == 'd':
+		if action == 'w':
 			self.player_pos += -8
-		elif action == 'l':
-			self.player_pos += 1
-		elif action == 'r':
+		elif action == 's':
+			self.player_pos += 8
+		elif action == 'a':
 			self.player_pos += -1
+		elif action == 'd':
+			self.player_pos += 1
 		
 		#self.world[self.player_pos[0]][[self.player_pos[1]]] = 'H'
 		#self.world[self.player_previous_pos[0]][self.player_previous_pos[1]] = ' '
 
+		self.render()
+
 		return [self.world, self.reward, self.game_over, None]
 
 	def reset(self):
+
+		self.screen.clear()
+		self.screen.border(0)
+
 		#resets to state
 		self.world = []
 		for x in range(self.world_size[0]*self.world_size[1]):
-			self.world.append([' '])
+			self.world.append(' ')
 
 		self.player_pos = self.start_pos
 		self.player_previous_pos = self.player_pos
@@ -55,48 +68,41 @@ class larryTheMeatEater_env(gym.Env):
 
 		#self.world[self.end_pos[0]][self.end_pos[1]] = 'R' # R for reward
 		
-		#self.game_over = False
-		#self.reward = 0
+		self.game_over = False
+		self.reward = 0
 		
 
 		return self.world
 
 	def render(self, mode='human', close=False):
-		# basically print the board
-		#for y in reversed(self.world):
-		#	for x in y:
-		#		print("[{}]".format(x), end ='')
-		#	print('\n')
-
-		#flat = self.flatten(self.world)
 
 		self.world[self.player_previous_pos] = ' '
 		self.world[self.player_pos] = 'H'
 		self.player_previous_pos = self.player_pos
 
-		# This will make the output overwritten in the terminal
-		print(	"[{}][{}][{}][{}][{}][{}][{}][{}]\n"
-			  	"[{}][{}][{}][{}][{}][{}][{}][{}]\n"
-				"[{}][{}][{}][{}][{}][{}][{}][{}]\n"
-				"[{}][{}][{}][{}][{}][{}][{}][{}]\n"
-				"[{}][{}][{}][{}][{}][{}][{}][{}]\n"
-				"[{}][{}][{}][{}][{}][{}][{}][{}]\n"
-				"[{}][{}][{}][{}][{}][{}][{}][{}]\n"
-				"[{}][{}][{}][{}][{}][{}][{}][{}]\n".format(
-					self.world[56],self.world[57],self.world[58],self.world[59],self.world[60],self.world[61],self.world[62],self.world[63],
-					self.world[48],self.world[49],self.world[50],self.world[51],self.world[52],self.world[53],self.world[54],self.world[55],
-					self.world[40],self.world[41],self.world[42],self.world[43],self.world[44],self.world[45],self.world[46],self.world[47],
-					self.world[32],self.world[33],self.world[34],self.world[35],self.world[36],self.world[37],self.world[38],self.world[39],
-					self.world[24],self.world[25],self.world[26],self.world[27],self.world[28],self.world[29],self.world[30],self.world[31],
-					self.world[16],self.world[17],self.world[18],self.world[19],self.world[20],self.world[21],self.world[22],self.world[23],
-					self.world[8] ,self.world[9] ,self.world[10],self.world[11],self.world[12],self.world[13],self.world[14],self.world[15],
-					self.world[0] ,self.world[1] ,self.world[2] ,self.world[3] ,self.world[4] ,self.world[5] ,self.world[6] ,self.world[7],), end='\r')
+		for y in range(self.world_size[0]):
+			for x in range(self.world_size[1]):
+				self.screen.addstr(1+y,1+x, self.world[y*8 + x] )
+
+		self.screen.refresh()
+
+	def quit(self):
+		curses.nocbreak()
+		self.screen.keypad(False)
+		curses.echo()
+
+		curses.endwin()
+		
 
 if __name__=='__main__':
+
 	env = larryTheMeatEater_env()
 	env.reset()
 	env.render()
-
-	#while True:
-	#	pass
+	try:
+		for x in range(5):
+			env.step('d')
+		env.quit()
+	except:
+		env.quit()
 	
