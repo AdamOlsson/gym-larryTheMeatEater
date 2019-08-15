4e721400a13b3579f7e43cc9a5f1363a1753c639
AdamOlsson <Olsson.95_@hotmail.com>
2019-08-15 13:41:00 +0200

    Added win condition & prevent player from going out of bounds

--- gym_larryTheMeatEater/envs/larryTheMeatEater_env.py
+++ gym_larryTheMeatEater/envs/larryTheMeatEater_env.py
@@ -10,13 +10,14 @@ class larryTheMeatEater_env(gym.Env):
 	def __init__(self):
 		
 		# World
-		self.world_size = (8,8)
-		self.world = None
-		self.start_pos = 0
-		self.end_pos = self.world_size[0]*self.world_size[1]
-		#self.death_objects_pos = []
-		self.player_pos = self.start_pos
-		self.player_previous_pos = None
+		self.world_size = [8,8]
+		self.world = []
+		self.start_pos = None
+		self.end_pos = None
+		self.death_pos = []
+
+		self.player_pos = [0, 0]
+		self.player_previous_pos = [0, 0]
 
 		# RL params
 		self.game_over = False
@@ -26,63 +27,91 @@ class larryTheMeatEater_env(gym.Env):
 		curses.initscr()
 		self.screen = curses.newwin(10,10)
 		self.screen.keypad(True)
+		curses.curs_set(0) 
 		curses.noecho()
 		curses.cbreak()
 
 
 	def step(self, action):
 
-		action = self.screen.getkey() # temporary for testing
+		self.player_previous_pos[0] = self.player_pos[0]
+		self.player_previous_pos[1] = self.player_pos[1]
 
-		# returns a list containing new state, the reward for current state, a bool saying game over and 
-		# additional info of our problem
 		if action == 'w':
-			self.player_pos += -8
+			self.player_pos[1] = max(0, self.player_pos[1]-1)
 		elif action == 's':
-			self.player_pos += 8
+			self.player_pos[1] = min(self.world_size[1]-1, self.player_pos[1]+1)
 		elif action == 'a':
-			self.player_pos += -1
+			self.player_pos[0] = max(0, self.player_pos[0]-1)
 		elif action == 'd':
-			self.player_pos += 1
+			self.player_pos[0] = min(self.world_size[0]-1, self.player_pos[0]+1)
 		
-		#self.world[self.player_pos[0]][[self.player_pos[1]]] = 'H'
-		#self.world[self.player_previous_pos[0]][self.player_previous_pos[1]] = ' '
-
+		if self.player_pos == self.end_pos:
+			self.game_over = True
+			#self.reset()
 		self.render()
 
 		return [self.world, self.reward, self.game_over, None]
 
 	def reset(self):
 
+		## TODO:: Add a load map fcn	
+
 		self.screen.clear()
 		self.screen.border(0)
+
+		self.start_pos = [0,0]
+		self.end_pos = [self.world_size[0]-1, self.world_size[1]-1]
 
 		#resets to state
 		self.world = []
 		for x in range(self.world_size[0]*self.world_size[1]):
 			self.world.append(' ')
 
-		self.player_pos = self.start_pos
-		self.player_previous_pos = self.player_pos
-		self.world[self.player_pos] = 'H'
+		# To pevent same address
+		self.player_pos[0] = self.start_pos[0]
+		self.player_pos[1] = self.start_pos[1]
+		self.player_previous_pos[0] = self.start_pos[0]
+		self.player_previous_pos[1] = self.start_pos[1]
 
-		#self.world[self.end_pos[0]][self.end_pos[1]] = 'R' # R for reward
+		self.world[self.player_pos[0] + self.player_pos[1]*8] = 'H'
+
+		# Put win location on board
+		win_pos = self.end_pos[0] + self.end_pos[1]*8
+		self.world[win_pos] = 'X'
+
+		## TODO: Put death locations on board
 		
 		self.game_over = False
 		self.reward = 0
 		
-
 		return self.world
+
+
+	def loadMap(self):
+		pass
+
 
 	def render(self, mode='human', close=False):
 
-		self.world[self.player_previous_pos] = ' '
-		self.world[self.player_pos] = 'H'
-		self.player_previous_pos = self.player_pos
+		# Remove old pos from board
+		player_prev_world_pos = self.player_previous_pos[0] + self.player_previous_pos[1]*8
+		self.world[player_prev_world_pos] = ' '
+
+		# Put in new pos on board
+		player_world_pos = self.player_pos[0] + self.player_pos[1]*8
+		self.world[player_world_pos] = 'H'
 
 		for y in range(self.world_size[0]):
 			for x in range(self.world_size[1]):
-				self.screen.addstr(1+y,1+x, self.world[y*8 + x] )
+				self.screen.addstr(1+y,1+x, self.world[y*8 + x])
+
+		## TODO: Add a game info box
+
+		#self.screen.addstr(4,2, "({},{})".format(self.end_pos[0], self.end_pos[1]))
+		#self.screen.addstr(5,2, "({},{})".format(self.player_pos[0], self.player_pos[1]))
+
+		#self.screen.addstr(8,8, u"\u2588")
 
 		self.screen.refresh()
 
@@ -90,7 +119,7 @@ class larryTheMeatEater_env(gym.Env):
 		curses.nocbreak()
 		self.screen.keypad(False)
 		curses.echo()
-
+		curses.curs_set(1)
 		curses.endwin()
 		
 
@@ -100,9 +129,13 @@ if __name__=='__main__':
 	env.reset()
 	env.render()
 	try:
-		for x in range(5):
-			env.step('d')
+		while True:
+			action = env.screen.getkey()
+			state = env.step(action)
+			if state[2] == True:
+				break
 		env.quit()
-	except:
+	except Exception as e:
 		env.quit()
+		print(e)
 	
